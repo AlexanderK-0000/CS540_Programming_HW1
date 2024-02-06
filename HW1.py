63eb2238cf8d4ede53774f152e65946f7c2068eb
Alexander Kerschen <Z237U349@wichita.edu>
2024-02-06 16:38:20 -0600

    Initial Commit

---
+++ HW1.py
@@ -0,0 +1,53 @@
+#Alexander Kerschen Z237U349
+import subprocess
+#subprocess.run() is being used to run the shell commands
+#it is condsidered more secure than os.system() (Check README.txt for more information)
+#shell=True make it so that whatever is put into the quotations marks is just inputed directly into the shell
+
+#Prints a message to the console if a command returns a non-zero code
+def ErrorHandling(output):
+    #subprocess.run returns a CompletedProcess Object (output)
+    #it has an attribute args, that is the command that was ran
+    #it has an attibute returncode, that is the value of the code returned after the command was executed (non-zero generally means an error occured)
+    if output.returncode != 0:
+        subprocess.run(f'echo Command "{output.args}" returned non-zero code: {output.returncode}', shell=True)
+
+#Loops until it hits the break in option 6
+while(True):
+    n = " & echo " #used as \n, more or less
+    #displays the menu using echo
+    subprocess.run(f"echo: & echo:{n}1)List directory contents{n}2)Print working directory{n}3)Create a new directory{n}4)Display a message{n}5)Concatenate and display content of a file{n}6)Exit & echo:", shell=True)
+
+    choice = input("Select an Option: ") #user chooses a menu option
+
+    subprocess.run("echo: ", shell=True)#makes an empty line
+     
+    #most of the command explanation I was going to make comments for just ended up as part of the explanation I print to the shell when the option is selected
+    #so if you want to know what the shell commads do, just read the string after the echo command in each if statement
+    if choice == "1": #List directory contents (dir/ls)
+        subprocess.run(f'echo Runs the command: "dir"{n}prints the contents of the current working directory{n}Entering a file path to a directory into dir as an argument will print the contents of that directry instead{n}Command Output:', shell=True)
+        output = subprocess.run("dir", shell=True) 
+        ErrorHandling(output)
+    elif choice == "2": #print working directory (cd/pwd)
+        subprocess.run(f'echo Runs the command: "cd"{n}This prints the file path to the working directory{n}Entering a file path into cd as an argument will move the working directory to that position{n}Command Output:', shell=True)
+        output = subprocess.run("cd", shell=True)
+        ErrorHandling(output)
+    elif choice == "3": #make a directory (mkdir)
+        path = input("Input the name of the new directory: ") 
+        subprocess.run(f'echo Runs the command: "mkdir <inputed_name>"{n}This creates a new directory called the previously inputed name in the current working directory{n}A file path can be specified before the name to create the directory in that position, rather than the working directory{n}Command Output:', shell=True)
+        output = subprocess.run(f"mkdir {path}", shell=True) 
+        ErrorHandling(output)
+    elif choice == "4": #display a message (echo)
+        message = input("Input the message you would like to display: ") 
+        subprocess.run(f'echo Runs the command: "echo <inputed_message>"{n}This prints the previously inputed message to the console{n}Command Output:', shell=True)
+        output = subprocess.run(f"echo {message}", shell=True)
+        ErrorHandling(output)
+    elif choice == "5": #concat/print a file (type/cat)
+        subprocess.run(f'echo Runs the command: "type sender.txt > reciever.txt | type reciever.txt"{n}"type sender.txt > reciever.txt" puts the contents of sender.txt into reciever.txt{n}"type reciever.txt" prints the contents of reciever.txt to the console{n}Command Output:', shell=True)
+        output = subprocess.run("type sender.txt > reciever.txt | type reciever.txt", shell=True)
+        ErrorHandling(output)
+    elif choice == "6": #Exit
+        break #Exits the loop, ending the program
+    else:#a number not 1-6
+        subprocess.run("echo Please choose a number from the menu.", shell=True)
+        
---
+++ README.txt
@@ -0,0 +1,38 @@
+Alexander Kerschen Z237U349
+
+Instructions for Running:
+	download and install python from: https://www.python.org/downloads/
+	Use cd to navigate into the directory containing HW1.py
+	enter "python HW1.py"
+	(Note: Using "python <file_path>/HW1.py" to run the script from outside the directory containing HW1.py, will cause option 5 (type/cat) to break)
+	(Also, reciever.txt starts as an empty text file, but will contain the contents of sender.txt after a single run of option 5)
+	Once the script is running
+		Option 1) Uses dir to print the contents of the working directory
+		Option 2) Uses cd to print the file path of the working directory
+		Option 3) Uses mkdir to create a new directory (will ask the user to input a name/path for the directory)
+		Option 4) Uses echo to print a message to the console (will ask the user for the message)
+		Option 5) Uses type to concatanating the contents of one file into another, and then print the contents of that file to the console
+		Option 6) Exits the program
+		
+subprocess.run() vs os.system()
+	subprocess.run() takes in the command you want to run as a list of strings
+		EX: subprocess.run(['dir','/d'])
+	os.system() takes a single string that it inputs directly to the shell as a command
+		EX: os.system('dir /d')
+	subprocess also has some additional functionality, like being able to save the output of the command
+	Security:
+		subprocess.run() is also condsidered more secure than os.system()
+		As far as I understand, this is because subprocess.run() is more resiliant to shell injection
+		For example:
+			arg = input("argument")
+			subprocess.run(['dir',arg])
+			os.system(f'dir {arg}')
+		Subprocess will only allow arg to be one argument because two or more arguments would have to be split into different list items
+		OS, on the otherhand, will just run whatever string arg ends up making f"dir {arg}"
+		So the user could input any number of things (like maybe a " & <entirely different command>") and os would just run it
+		Something to note, I use subprocess's shell=True parameter in the homework
+		This essentially makes it work like os
+			EX: subprocess.run("dir /d", shell=True)
+		Therefore, I'm really not getting the security benefit
+		(I used shell=True for simplicity)
+		(also, without shell=True, subprocess has some very annoying issues with running correctly on Windows, so I just avoided them)
